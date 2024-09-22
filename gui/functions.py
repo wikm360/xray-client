3b67808f19876bfea7cc08803bca7be9efa25b5b
3ircle <150266401+3ircle@users.noreply.github.com>
2024-09-22 22:56:17 +0330

    profile list
    added profile list

--- gui/functions.py
+++ gui/functions.py
@@ -1,2 +1,7 @@
-def Import_btn():
-    pass
+def Import_btn(url_box):
+    print(url_box.get())
+def add_list(List:list,list_box):
+    c = 1
+    for i in List:
+        list_box.insert(c,i)
+        c +=1
--- gui/mian.py
+++ gui/mian.py
@@ -2,12 +2,14 @@ from tkinter import *
 from functions import *
 import json
 
-theme_mode = "dark_mode"
+theme_mode = "light_mode"
 
 
-with open("./var.json","r") as f:
+with open("./setting.json","r") as f:
     var_json = json.loads(f.read())
 root = Tk()
+
+
 
 root.geometry("900x600")
 root.title(var_json["title"])
@@ -17,13 +19,14 @@ root.config(bg=var_json[theme_mode]["bg"])
 url_box = Entry(root,font=("Helvetica", 13))
 url_box.place(relx=0.019440124,rely=0.021879022,relwidth=0.461119751,relheight=0.069498069)
 
-import_btn = Button(root,text="import",command=Import_btn,bg=var_json[theme_mode]["import_btn"])
+import_btn = Button(root,text="import",command=lambda:Import_btn(url_box),bg=var_json[theme_mode]["import_btn"])
 import_btn.place(relx=0.488335925,rely=0.021879022,relwidth=0.081648523,relheight=0.069498069)
 
+profile_list = Listbox(root,selectbackground="#2d9bf0",font=("Helvetica",20),border=5)
+profile_list.place(relx=0.020217729,rely=0.145431145,relwidth=0.239502333,relheight=0.791505792)
+l = ["profile 1","profile 2","profile 3","profile 4","profile 5"]
 
-
-
-
+add_list(l,profile_list)
 
 
 
