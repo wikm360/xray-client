e8df20e3ff682b351903a274d5eabbba076f8d37
Mohammad Shafiee <51268970+wikm360@users.noreply.github.com>
2024-09-24 20:12:01 +0330

    improve gui / add refresh button

--- gui/functions.py
+++ gui/functions.py
@@ -1,7 +1,76 @@
+import os
+import sys
+from tkinter import messagebox
+import requests
+import shutil
+import base64
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import convert
+import json
+
+def on_entry_click(event, entry, placeholder):
+    if entry.get() == placeholder:
+        entry.delete(0, "end")
+        entry.config(fg='black')
+
+def on_focusout(event, entry, placeholder):
+    if entry.get() == "":
+        entry.insert(0, placeholder)
+        entry.config(fg='grey')
+
+
 def Import_btn(url_box):
     print(url_box.get())
-def add_list(List:list,list_box):
-    c = 1
+def add_list(List:list, list_box):
     for i in List:
-        list_box.insert(c,i)
-        c +=1
+        list_box.insert('end', i)
+def sub_refresh(profile_list):
+    l = []
+    path = "./subs"
+    directories = [d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))]
+    
+    if directories:
+        for sub in directories:
+            l.append(sub)
+
+    profile_list.delete(0, 'end')
+    add_list(l, profile_list)
+
+def get_sub(left_box,right_box):
+    sub_url = right_box.get()
+    sub_name = left_box.get()
+
+    if not sub_url or not sub_name:
+        messagebox.showwarning("Warning", "Please fill in all fields.")
+        return
+
+    headers = {"user-agent": "v2rayNG"}
+    try:
+        r = requests.get(url=sub_url, headers=headers)
+        text = r.text
+        decoded_bytes = base64.b64decode(text)
+        decoded_str = decoded_bytes.decode('utf-8')
+        list_configs = decoded_str.split("\n")
+        
+        directory_path = f"./subs/{sub_name}"
+        if os.path.exists(directory_path) and os.path.isdir(directory_path):
+            shutil.rmtree(directory_path)  # حذف پوشه قدیمی
+            messagebox.showinfo("Success", f"Previous {sub_name} sub deleted ...")
+        
+        os.mkdir(directory_path)  # ایجاد پوشه جدید
+
+        dict_name = {}
+        for count, config in enumerate(list_configs):
+            if config.strip():
+                config_json, config_name = convert.convert(config)
+                if config_name != "False":
+                    with open(f"./subs/{sub_name}/{count}.json", "w") as f:
+                        f.write(config_json)
+                    dict_name[count] = config_name
+        
+        with open(f"./subs/{sub_name}/list.json", "w", encoding="utf-8") as f:
+            json.dump(dict_name, f, ensure_ascii=False, indent=4)
+        
+        messagebox.showinfo("Success", f"Subscription {sub_name} added successfully!")
+    except Exception as e:
+        messagebox.showerror("Error", str(e))
--- gui/mian.py
+++ gui/mian.py
@@ -1,7 +1,6 @@
 from tkinter import *
 from functions import *
 import json
-import os
 
 theme_mode = "light_mode"
 
@@ -11,24 +10,9 @@ with open("./setting.json","r") as f:
 root = Tk()
 
 
-
 root.geometry("900x600")
 root.title(var_json["title"])
 root.config(bg=var_json[theme_mode]["bg"])
-
-
-# url_box = Entry(root,font=("Helvetica", 13))
-# url_box.place(relx=0.019440124,rely=0.021879022,relwidth=0.461119751,relheight=0.069498069)
-
-def on_entry_click(event, entry, placeholder):
-    if entry.get() == placeholder:
-        entry.delete(0, "end")
-        entry.config(fg='black')
-
-def on_focusout(event, entry, placeholder):
-    if entry.get() == "":
-        entry.insert(0, placeholder)
-        entry.config(fg='grey')
 
 left_box = Entry(root, font=("Helvetica", 13), fg='grey')
 left_box.insert(0, "name")
@@ -42,28 +26,16 @@ right_box.bind("<FocusIn>", lambda event: on_entry_click(event, right_box, "subs
 right_box.bind("<FocusOut>", lambda event: on_focusout(event, right_box, "subscription link"))
 right_box.place(relx=0.32, rely=0.021879022, relwidth=0.45, relheight=0.069498069)
 
-import_btn = Button(root, text="import", command=lambda: Import_btn(right_box), bg=var_json[theme_mode]["import_btn"])
+import_btn = Button(root, text="import", command=lambda: get_sub(left_box, right_box), bg=var_json[theme_mode]["import_btn"])
 import_btn.place(relx=0.77, rely=0.021879022, relwidth=0.081648523, relheight=0.069498069)
 
 
 profile_list = Listbox(root,selectbackground="#2d9bf0",font=("Helvetica",20),border=5)
 profile_list.place(relx=0.020217729,rely=0.145431145,relwidth=0.239502333,relheight=0.791505792)
 
-l = []
-path = "./subs"
-directories = [d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))]
-if directories:
-    for i, sub in enumerate(directories, 1):
-        l.append(sub)
-# l = ["profile 1","profile 2","profile 3","profile 4","profile 5"]
+refresh_btn = Button(root, text="Refresh", command=lambda: sub_refresh(profile_list), bg=var_json[theme_mode]["import_btn"])
+refresh_btn.place(relx=0.020217729, rely=0.95, relwidth=0.239502333, relheight=0.05)
 
-add_list(l,profile_list)
-
-root.mainloop()
-
-
-
-
-
+sub_refresh(profile_list)
 
 root.mainloop()
