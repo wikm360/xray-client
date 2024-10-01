749a97d311c63e849938a7d8fc057814a8682ae3
Mohammad Shafiee <51268970+wikm360@users.noreply.github.com>
2024-10-01 20:40:57 +0330

    add spithttp support for vmess

--- gui/new_gui.py
+++ gui/new_gui.py
@@ -11,7 +11,7 @@ from tkinter import *
 
 
 OUTPUT_PATH = Path(__file__).parent
-ASSETS_PATH = OUTPUT_PATH / Path(r"/home/ali/Documents/gui_xcore/build/assets/frame0")
+ASSETS_PATH = OUTPUT_PATH / Path(r"/home/ali/Documents/gui_xcore/build/assets/frame0") # /home/wikmgg/Documents/GitHub/xray-client/gui/assets/frame0
 
 
 def relative_to_assets(path: str) -> Path:
