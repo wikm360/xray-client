9176236d03ffb433de4888d054fb7e08c52e435f
Mohammad Shafiee <51268970+wikm360@users.noreply.github.com>
2024-10-03 07:47:50 +0330

    git

--- gui/main.py
+++ gui/main.py
@@ -18,12 +18,7 @@ theme_mode = var_json["mode"]
 
 
 root = Tk()
-<<<<<<< Updated upstream
-# print(os.path.isfile("./gui/XC.ico"))
-# root.iconbitmap("./gui/XC.ico")
-=======
-# root.iconbitmap("XC.ico")
->>>>>>> Stashed changes
+
 
 consol_text = StringVar()
 
