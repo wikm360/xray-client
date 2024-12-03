b032367901162a5ad9787f089aa332164de382e3
Mohammad Shafiee <51268970+wikm360@users.noreply.github.com>
2024-12-03 11:36:59 +0330

    Update CLI

--- CLI/convert.py
+++ CLI/convert.py
@@ -71,7 +71,8 @@ def decode_vmess(link):
     elif config['net'] == "splithttp":
         stream_settings["splithttpSettings"] = {
             "host": config.get('host', ""),
-            "path": config.get('path', "/")
+            "path": config.get('path', "/"),
+            "xmux" :{"maxConcurrency": 4, "cMaxLifetimeMs": 10000}
         }
 
     if config.get('tls') == "tls":
@@ -86,12 +87,18 @@ def decode_vmess(link):
     xray_config = {
         "log": {"loglevel": "info"},
         "inbounds": [{
+            "tag": "socks",
             "port": 1080,
-            "protocol": "socks",
+            "listen": "127.0.0.1" ,
+            "protocol": "socks" ,
+            "sniffing": {
+                "enabled": True ,
+                "destOverride": ["http","tls"],
+                "routeOnly": False} ,
             "settings": {
                 "auth": "noauth",
-                "udp": True
-            }
+                "udp": True ,
+                "allowTransparent": False}
         }],
         "outbounds": [{
             "protocol": "vmess",
@@ -111,7 +118,52 @@ def decode_vmess(link):
             },
             "streamSettings": stream_settings,
             "tag": "proxy"
-        }]
+        }],
+        "dns": {
+            "hosts": {
+            "dns.google": "8.8.8.8"
+            },
+            "servers": [
+            {
+                "address": "223.5.5.5",
+                "domains": [
+                "wikm.ir"
+                ]
+            },
+            "1.1.1.1",
+            "8.8.8.8",
+            "https://dns.google/dns-query",
+            {
+                "address": "223.5.5.5",
+                "domains": [
+                address
+                ]
+            }
+            ]
+        },
+        "routing": {
+            "domainStrategy": "AsIs",
+            "rules": [
+            {
+                "type": "field",
+                "inboundTag": [
+                "api"
+                ],
+                "outboundTag": "api"
+            },
+            {
+                "type": "field",
+                "port": "443",
+                "network": "udp",
+                "outboundTag": "block"
+            },
+            {
+                "type": "field",
+                "port": "0-65535",
+                "outboundTag": "proxy"
+            }
+            ]
+        }
     }
 
     return json.dumps(xray_config, indent=4), config_name
@@ -146,6 +198,13 @@ def decode_vless(link):
                     }
                 }
             }
+        }
+
+    elif stream_settings['network'] == "splithttp":
+        stream_settings["splithttpSettings"] = {
+            "host": query_params.get('host', [""])[0],
+            "path": query_params.get('path', ["/"])[0],
+            "xmux" :{"maxConcurrency": 4, "cMaxLifetimeMs": 10000}
         }
 
     elif stream_settings["network"] == "ws":
@@ -184,7 +243,7 @@ def decode_vless(link):
     xray_config = {
         "log": {"loglevel": "info"},
         "inbounds": [{
-            "port": 1080, "protocol": "socks", "settings": {"auth": "noauth", "udp": True}
+            "tag": "socks", "port": 1080, "listen": "127.0.0.1" , "protocol": "socks" , "sniffing": {"enabled": True ,"destOverride": ["http","tls"],"routeOnly": False} , "settings": {"auth": "noauth", "udp": True , "allowTransparent": False}
         }],
         "outbounds": [{
             "protocol": "vless",
@@ -195,7 +254,52 @@ def decode_vless(link):
                 }]
             },
             "streamSettings": stream_settings
-        }]
+        }],
+        "dns": {
+            "hosts": {
+            "dns.google": "8.8.8.8"
+            },
+            "servers": [
+            {
+                "address": "223.5.5.5",
+                "domains": [
+                "wikm.ir"
+                ]
+            },
+            "1.1.1.1",
+            "8.8.8.8",
+            "https://dns.google/dns-query",
+            {
+                "address": "223.5.5.5",
+                "domains": [
+                address
+                ]
+            }
+            ]
+        },
+        "routing": {
+            "domainStrategy": "AsIs",
+            "rules": [
+            {
+                "type": "field",
+                "inboundTag": [
+                "api"
+                ],
+                "outboundTag": "api"
+            },
+            {
+                "type": "field",
+                "port": "443",
+                "network": "udp",
+                "outboundTag": "block"
+            },
+            {
+                "type": "field",
+                "port": "0-65535",
+                "outboundTag": "proxy"
+            }
+            ]
+        }
     }
     return json.dumps(xray_config, indent=4)
 
